3d84b98a16ca2f843f9f64683edd6d8b6a208de1
Alexandre Rodrigues <btt.ale@gmail.com>
2021-07-14 14:38:21 -0300

    webpack config finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 let mode = 'development';
 
@@ -8,6 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
+    devtool: 'source-map',
     mode: mode,
     devServer:{
         hot: true,
@@ -19,12 +22,42 @@ module.exports = {
     entry: './src/js/index.js',
     output:{
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename:'images/[hash][ext][query]'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset'
+            },
+            {
+                test:/\.s?css$/i,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                     'sass-loader'],
+            },
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     },
     plugins:[
         new HtmlWebpackPlugin({
             filename:'index.html',
             template: './src/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename:'bundle.css'
+        }),
+        new CleanWebpackPlugin()
     ]
 }
